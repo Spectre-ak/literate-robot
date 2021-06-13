64dd8f9cf912355a1f00892945dd35703bc4f814
Spectre-ak <62694340+Spectre-ak@users.noreply.github.com>
2021-06-13 20:24:40 +0530

    added image upload template

--- app/app.py
+++ app/app.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask,render_template, request
 import run_model_memory
+import uuid
 
 app = Flask(__name__)
 
 @app.route("/")
 def helloworld():
-    return "Hello World!"
+    print(str(uuid.uuid4()))
+    return render_template("uploadImage.html")
 
 @app.route("/ru")
 def helloworqld():
@@ -15,6 +17,17 @@ def helloworqld():
 def runModelRest():
     run_model_memory.run()
     return "running"
+
+@app.route('/upload', methods = ['GET', 'POST'])
+def upload_file():
+   if request.method == 'POST':
+      f = request.files['file']
+      name=str(uuid.uuid4())+".jpg"
+      print(name)
+      f.save((name))
+      image=run_model_memory.run(name)
+      res={"data":image}
+      return res
 
 app.run(debug=True, host='0.0.0.0',port=5000)
 
